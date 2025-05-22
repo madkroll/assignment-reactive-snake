64a006fcb9cb672f1024a23d8ec066a697648da9
Maxim Nikitin <mnikitin.official@gmail.com>
2025-05-22 15:29:07 +0200

    Render the grid dynamically from the server. Tech-debt: ignoring CORS

--- snake-backend/snake-web/src/main/java/com/madkroll/snake/GameController.java
+++ snake-backend/snake-web/src/main/java/com/madkroll/snake/GameController.java
@@ -1,18 +1,23 @@
 package com.madkroll.snake;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+// TODO: fix it by putting reversed proxy in front of client and server
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController
 @EnableWebMvc
 @RequestMapping("/game")
 public class GameController {
 
     @GetMapping(
-            path = "/start"
+            path = "/start",
+            produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<GameSession> startNewGame() {
         return ResponseEntity.ok(new GameSession(new Grid(10, 10)));
